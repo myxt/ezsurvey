ec0e5c6aafe01087a51c459310871864ef3ee89a
svda <sander@myxt.nl>
2011-10-21 16:31:34 +0200

    Syncing with latest version.

--- translations/dut-NL/translation.ts
+++ translations/dut-NL/translation.ts
@@ -2,6 +2,17 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
+    <name>design/admin/content/edit</name>
+    <message>
+        <source>View and manage (copy, delete, etc.) the versions of this object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Manage versions</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>design/admin/node/view/full</name>
     <message>
         <source>Last modified</source>
@@ -63,6 +74,10 @@
         <source>Preview</source>
         <translation>Bekijken</translation>
     </message>
+    <message>
+        <source>New translation</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>design/admin/parts/survey/menu</name>
@@ -81,18 +96,6 @@
 </context>
 <context>
     <name>design/ezwebin/full/article</name>
-    <message>
-        <source>Comments</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>New comment</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to comment.</source>
-        <translation type="unfinished"></translation>
-    </message>
     <message>
         <source>Tip a friend</source>
         <translation type="unfinished"></translation>
@@ -314,11 +317,6 @@
     <message>
         <source>Receiver</source>
         <translation>Ontvanger</translation>
-    </message>
-    <message>
-        <source>N. B. If you enter just one email address, user will not see this question. Instead the posting
-will be directly sent to the address.</source>
-        <translation>N,B, Wanneer slechts één e-mailadres wordt ingevoerd dan wordt de vraag niet getoond. In plaats daarvan wordt het formulier direct naar het ingevulde adres gestuurd.</translation>
     </message>
     <message>
         <source>Name</source>
@@ -822,6 +820,103 @@ will be directly sent to the address.</source>
     </message>
     <message>
         <source>All values in Valid to need to be numeric.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Feedback field entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send copy in bcc to admin (%1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mapping</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text of subject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Feedback message for the receiver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Email Sender</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>N. B. If you enter just one email address, user will not see this question. Instead the posting
+  will be directly sent to the address.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Time entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Now</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter a valid date!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Email Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Feedback field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Form Receiver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Related object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Section Header</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter a valid time!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Transaction Entry</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
